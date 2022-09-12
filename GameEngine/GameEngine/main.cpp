8d389790cd3962eb141c915c69f9992e7d1b1575
BozhidarVelikov <bojidarvelikov2@gmail.com>
2022-09-12 14:19:10 +0100

    Added libraries to the project
    Added the following libraries to the project: Box2D, FMOD, GLFW, glm, imgui

--- GameEngine/GameEngine/main.cpp
+++ GameEngine/GameEngine/main.cpp
@@ -1,7 +1,43 @@
-#include <test.h>
+#include <iostream>
+
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <fmod.hpp>
+
+#include <box2d/box2d.h>
 
 int main() {
-	std::cout << "Hello world!" << std::endl;
+	// Test GLFW
+	if (!glfwInit()) {
+		std::cout << "Fail!" << std::endl;
+	}
+	else {
+		std::cout << "Success!" << std::endl;
+	}
+
+	// Test imgui
+	IMGUI_CHECKVERSION();
+
+	// Test glm
+	std::cout << glm::abs(-5) << std::endl;
+
+	// Test FMOD
+	const int MAX_CHANNELS = 64;
+	FMOD::System *system;
+
+	FMOD::System_Create(&system);
+	system -> init(MAX_CHANNELS, FMOD_INIT_NORMAL, NULL);
+
+	// Test Box2D
+	b2Body body();
 
 	return 0;
 }
