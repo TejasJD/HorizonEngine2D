3a60763b818ff8fa4b0f49894b9121516d606954
TejasJD <ddusane78@outlook.com>
2022-09-22 22:41:17 +0100

    Updated Generator to Visual Studio 17 2022, Added 'GLFW' and 'spdlog' libraries to the project

--- HorizonEngine/HorizonEngine.cpp
+++ HorizonEngine/HorizonEngine.cpp
@@ -1,12 +1,21 @@
 ﻿// HorizonEngine.cpp : Defines the entry point for the application.
 //
 
+#include <memory>
 #include "HorizonEngine.h"
-
-using namespace std;
+#include "GLFW/glfw3.h"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	// Test CMake.
+	std::cout << "Hello CMake." << std::endl;
+
+	// Test GLFW
+	std::cout << (glfwInit() ? "success" : "failure") << std::endl;
+
+	// Test spdlog
+	spdlog::info("Welcome to spdlog!");
 	return 0;
 }
