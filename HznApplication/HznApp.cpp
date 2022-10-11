6ad4ac08caa0688a7011e69af867ee40e46b2aac
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2022-10-11 15:04:23 +0100

    Created menu toolbar docked to the top of the screen

--- HznApplication/HznApp.cpp
+++ HznApplication/HznApp.cpp
@@ -4,6 +4,7 @@
 #include "imgui_impl_opengl3.h"
 #include "HorizonEngine.h"
 #include "HznApp.h"
+#include "Window.h"
 
 std::shared_ptr<Hzn::App> Hzn::createApp()
 {
@@ -49,7 +50,7 @@ void SampleLayer::onEvent(Hzn::Event& event)
 	}
 }
 
-// ********************
+// ************************************************************************
 
 // *********** EDITOR LAYER **********
 EditorLayer::EditorLayer(const std::string& name) : Layer(name) {}
@@ -61,53 +62,88 @@ void EditorLayer::onAttach()
 
 void EditorLayer::onRenderImgui()
 {
-	static bool my_tool_active = true;
-	ImGui::Begin("From Editor Layer", &my_tool_active, ImGuiWindowFlags_MenuBar);
-	if (ImGui::BeginMenuBar())
+	// ImGuiStyle& style = ImGui::GetStyle();
+	// style.Colors[ImGuiCol_WindowBg] = ImVec4(255, 0, 0, 255); // RGB Values 
+
+	// static bool my_tool_active = true;
+	// ImGui::Begin("From Editor Layer", &my_tool_active, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
+	// ImGui::SetNextWindowSize(ImVec2(50,500));
+
+	ImVec2 vWindowSize = ImGui::GetMainViewport()->Size;
+	ImVec2 vPos0 = ImGui::GetMainViewport()->Pos;
+
+	ImGui::SetNextWindowPos(ImVec2((float)vPos0.x, (float)vPos0.y), ImGuiCond_Always);
+	ImGui::SetNextWindowSize(ImVec2((float)vWindowSize.x, (float)vWindowSize.y));
+
+	if (ImGui::Begin(
+		"From Editor Layer",
+		nullptr,
+		ImGuiWindowFlags_MenuBar |
+		ImGuiWindowFlags_NoResize |
+		ImGuiWindowFlags_NoMove |
+		ImGuiWindowFlags_NoCollapse |
+		ImGuiWindowFlags_NoBringToFrontOnFocus |
+		ImGuiWindowFlags_NoTitleBar
+	)
+		)
+
 	{
-		if (ImGui::BeginMenu("File"))
 		{
-			if (ImGui::MenuItem("Open..", "Ctrl+O")) { /* Do stuff */ }
-			if (ImGui::MenuItem("Save", "Ctrl+S")) { /* Do stuff */ }
-			if (ImGui::MenuItem("Close", "Ctrl+W")) { my_tool_active = false; }
-			ImGui::EndMenu();
+			static const ImGuiDockNodeFlags dockspaceFlags = ImGuiDockNodeFlags_None;
+			ImGuiID dockSpace = ImGui::GetID("MainWindowDockspace");
+			ImGui::DockSpace(dockSpace, ImVec2(0.0f, 0.0f), dockspaceFlags);
+
+			if (ImGui::BeginMainMenuBar())
+			{
+				if (ImGui::BeginMenu("File"))
+				{
+					if (ImGui::MenuItem("Open")){ /* Do Something */ }
+					if (ImGui::MenuItem("Save")){ /* Do Something */ }
+					if (ImGui::MenuItem("Close")){ /* Do Something */ }
+					ImGui::EndMenu();
+				}
+				if (ImGui::BeginMenu("Edit"))
+				{
+					if(ImGui::MenuItem("Undo")){ /* Do Something */ }
+					if(ImGui::MenuItem("Redo")){ /* Do Something */ }
+
+					if(ImGui::MenuItem("Cut")){ /*Do Something*/ }
+					if (ImGui::MenuItem("Copy")) { /*Do Something*/ }
+					if (ImGui::MenuItem("Paste")) { /*Do Something*/ }
+					ImGui::EndMenu();
+				}
+				if (ImGui::BeginMenu("Assets"))
+				{
+					if(ImGui::MenuItem("Asset Submenu_1")){ /* Do Something */ }
+					ImGui::EndMenu();
+				}
+				if (ImGui::BeginMenu("Game Objects"))
+				{
+					if (ImGui::MenuItem("Game Object Submenu_1")) { /* Do Something */ }
+					ImGui::EndMenu();
+				}
+				if (ImGui::BeginMenu("Components"))
+				{
+					if (ImGui::MenuItem("Components Submenu_1")) { /* Do Something */ }
+					ImGui::EndMenu();
+				}
+				if (ImGui::MenuItem("Window"))
+				{
+					if (ImGui::MenuItem("Window Submenu_1")) { /* Do Something */ }
+					ImGui::EndMenu();
+				}
+				if (ImGui::BeginMenu("Help"))
+				{
+					if (ImGui::MenuItem("Help Submenu_1")) { /* Do Something */ }
+					ImGui::EndMenu();
+				}
+			}
+			ImGui::EndMainMenuBar();
 		}
-		if (ImGui::BeginMenu("Edit"))
-		{
-			if (ImGui::MenuItem("Copy", "Ctrl+C")) { /* Do Stuff */ }
-			if (ImGui::MenuItem("Cut", "Ctrl+X")) { /* Do Stuff */ }
-			if (ImGui::MenuItem("Paste", "Ctrl+P")) { /* Do Stuff */ }
-			ImGui::EndMenu();
-		}
-		if (ImGui::BeginMenu("Assets"))
-		{
-			if (ImGui::MenuItem("Asset Sub Menu 1", "Ctrl+A")) { /* Do Stuff */ }
-			ImGui::EndMenu();
-		}
-		if (ImGui::BeginMenu("Game Objects"))
-		{
-			if (ImGui::MenuItem("Game Objects Sub Menu 1", "Ctrl+A")) { /* Do Stuff */ }
-			ImGui::EndMenu();
-		}
-		if (ImGui::BeginMenu("Component"))
-		{
-			if (ImGui::MenuItem("Asset Sub Menu 1", "Ctrl+A")) { /* Do Stuff */ }
-			ImGui::EndMenu();
-		}
-		if (ImGui::BeginMenu("Window"))
-		{
-			if (ImGui::MenuItem("Window Sub Menu 1", "Ctrl+A")) { /* Do Stuff */ }
-			ImGui::EndMenu();
-		}
-		if (ImGui::BeginMenu("Help"))
-		{
-			if (ImGui::MenuItem("Help Sub Menu 1", "Ctrl+A")) { /* Do Stuff */ }
-			ImGui::EndMenu();
-		}
-		ImGui::EndMenuBar();
 	}
 	ImGui::End();
 }
+
 
 void EditorLayer::onDetach() {}
 
