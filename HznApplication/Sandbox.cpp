f824245dd2cbb207a0462e79882208d0eb59b05d
TejasJD <ddusane78@outlook.com>
2022-10-28 02:02:46 +0100

    Added wrap around behaviour when colliding with edges, added UI component to start and stop snake game, added max score, Optimized snake game.

--- HznApplication/Sandbox.cpp
+++ HznApplication/Sandbox.cpp
@@ -2,24 +2,60 @@
 #include <HorizonEngine.h>
 #include "Sandbox.h"
 
+static std::pair<int, int>& operator+=(std::pair<int, int>& a, const std::pair<int, int>& b)
+{
+	a.first += b.first;
+	a.second += b.second;
+	return a;
+}
+
+static std::pair<int, int> operator+(const std::pair<int, int>& a, const std::pair<int, int>& b)
+{
+	std::pair<int, int> temp = a;
+	temp += b;
+	return temp;
+}
+
+static std::pair<int, int>& operator%=(std::pair<int, int>& a, const int& b)
+{
+	a.first %= b;
+	a.second %= b;
+	return a;
+}
+
+static std::pair<int, int> operator%(const std::pair<int, int>& a, const int& b)
+{
+	std::pair<int, int> temp = a;
+	temp %= b;
+	return temp;
+}
+
 Sandbox::Sandbox(const std::string& name)
 	:cameraController(Hzn::OrthographicCameraController(
 		(float)Hzn::App::getApp().getAppWindow().getWidth() / (float)Hzn::App::getApp().getAppWindow().getHeight(), 1.0f))
 {
-	/*cameraController.enableMouseDragMovement(true);
-	cameraController.enableRotation(true);*/
-
+	recalculateFreePositions();
 	food = generateFood();
 }
 
 void Sandbox::onUpdate(Hzn::TimeStep deltaTime)
 {
+	/*HZN_INFO("{0}, {1}", food.first, food.second);*/
+	//HZN_DEBUG("{0}", freePositions.size());
+	/*for (const auto& x : snake)
+	{
+		std::cout << "(" << x.first << ", " << x.second << ") ";
+	}
+	std::cout << std::endl;*/
 	calculateFps();
+	/*cameraController.onUpdate(deltaTime);*/
 	cameraController.getCamera().setPosition(glm::vec3{ gridSize / 2 * 0.11f, gridSize / 2 * 0.11f, 0.0f });
 	Hzn::RenderCall::setClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
 	Hzn::RenderCall::submitClear();
 
 	Hzn::Renderer2D::beginScene(dynamic_cast<const Hzn::OrthographicCamera&>(cameraController.getCamera()));
+
+	if(gridSize != oldGridSize) recalculateFreePositions();
 
 	// if the food goes out of the grid, generate new food.
 	if (food.first >= gridSize || food.second >= gridSize)
@@ -29,97 +65,76 @@ void Sandbox::onUpdate(Hzn::TimeStep deltaTime)
 
 	// update snake position
 
-	float curTime = glfwGetTime();
-	if (curTime - lTime >= 0.5f)
+	if (startGame)
 	{
-		std::pair<int, int> moveDirection = {0, 0};
-		if (direction == Direction::Up) {
-			moveDirection = { 0, 1 };
-		}
-		else if (direction == Direction::Down) {
-			moveDirection = { 0, -1 };
-		}
-		else if (direction == Direction::Left) {
-			moveDirection = { -1, 0 };
-		}
-		else {
-			moveDirection = { 1, 0 };
-		}
-		if (snakeSize.back().first + moveDirection.first < 0 
-			|| snakeSize.back().first + moveDirection.first >= gridSize
-			|| snakeSize.back().second + moveDirection.second < 0
-			|| snakeSize.back().second + moveDirection.second >= gridSize
-			|| isCollidingWithItself({ snakeSize.back().first + moveDirection.first, snakeSize.back().second + moveDirection.second }))
+		float curTime = glfwGetTime();
+		if (curTime - lTime >= 0.5f)
 		{
-			snakeSize = { {0, 0}, {0, 1}, {0, 2} };
-			direction = Direction::Up;
-			food = generateFood();
-			score = 0;
-		}
-		else {
-			snakeSize.push_back({ snakeSize.back().first + moveDirection.first, snakeSize.back().second + moveDirection.second });
+			std::pair<int, int> moveDirection = { 0, 0 };
+			if (direction == Direction::Up) moveDirection = { 0, 1 };
+			else if (direction == Direction::Down) moveDirection = { 0, -1 };
+			else if (direction == Direction::Left) moveDirection = { -1, 0 };
+			else if (direction == Direction::Right) moveDirection = { 1, 0 };
 
-			if (snakeSize.back().first == food.first && snakeSize.back().second == food.second) {
-				food = generateFood();
+			if (isCollidingWithItself(moveDirection))
+			{
+				recalculateFreePositions();
+			}
+			else
+			{
+				// move the snake
+				snakeHead = ((snakeHead + moveDirection) % gridSize + std::make_pair(gridSize, gridSize)) % gridSize;
+				// add the snakeHead to the snake
+				snake.push_back(snakeHead);
+				// get the previous free position from the map.
+				auto it = freePositions.find(snakeHead);
+				// free the previous free position index from the array.
+				freePositions[freePositionsArray.back()] = it->second;
+				swap(freePositionsArray[it->second], freePositionsArray.back());
+				freePositionsArray.pop_back();
+				// remove the previous free position from the map as well.
+				freePositions.erase(it);
 
-				if (victory) {
-					snakeSize = { {0, 0}, {0, 1}, {0, 2} };
-					direction = Direction::Up;
+				// if snake eats food.
+				if (snakeHead == food)
+				{
+					score++;
+					maxScore = std::max(score, maxScore);
 					food = generateFood();
-					victory = false;
-					score = 0;
 				}
-				
-				score++;
+				else
+				{
+					// push the upcoming free position in the freePositions array.
+					freePositions.emplace(snake.front(), (int)freePositions.size() - 1);
+					freePositionsArray.push_back(snake.front());
+					// free the position.
+					// pop the snake tail.
+					snake.pop_front();
+				}
+				snakeHead = snake.back();
+			}
 
-			}
-			else {
-				snakeSize.pop_front();
-			}
+			lTime = curTime;
 		}
 
-		lTime = curTime;
-	}
-
-
-	if (Hzn::Input::keyPressed(Hzn::Key::Up))
-	{
-		direction = Direction::Up;
-	}
-	else if (Hzn::Input::keyPressed(Hzn::Key::Down))
-	{
-		direction = Direction::Down;
-	}
-	else if (Hzn::Input::keyPressed(Hzn::Key::Left))
-	{
-		direction = Direction::Left;
-	}
-	else if (Hzn::Input::keyPressed(Hzn::Key::Right))
-	{
-		direction = Direction::Right;
+		processSnakeInput();
 	}
 
 	for (int i = 0; i < gridSize; ++i)
 	{
 		for (int j = 0; j < gridSize; ++j)
 		{
-			if (i == food.first && j == food.second) {
-				Hzn::Renderer2D::drawQuad(glm::vec3((float)i * 0.11f, (float)j * 0.11f, 0.0f), glm::vec3(0.1f)
-					, glm::vec4(foodColor, 1.0f));
+			if (!freePositions.count({ i, j }))
+			{
+				Hzn::Renderer2D::drawQuad({ i * 0.11, j * 0.11 }, glm::vec4(0.1f), glm::vec4(snakeColor, 1.0f));
 			}
-			else {
-				bool snakeFound = false;
-				for (int k = 0; k < snakeSize.size(); ++k)
+			else
+			{
+				if (i == food.first && j == food.second)
 				{
-					if (i == snakeSize[k].first && j == snakeSize[k].second)
-					{
-						Hzn::Renderer2D::drawQuad(glm::vec3((float)i * 0.11f, (float)j * 0.11f, 0.0f), glm::vec3(0.1f)
-							, glm::vec4(snakeColor, 1.0f));
-						snakeFound = true;
-						break;
-					}
+					Hzn::Renderer2D::drawQuad({ i * 0.11, j * 0.11 }, glm::vec4(0.1f), glm::vec4(foodColor, 1.0f));
 				}
-				if (!snakeFound) Hzn::Renderer2D::drawQuad(glm::vec3((float)i * 0.11f, (float)j * 0.11f, 0.0f), glm::vec3(0.1f));
+				else Hzn::Renderer2D::drawQuad({ i * 0.11, j * 0.11}, glm::vec4(0.1f));
 			}
 		}
 	}
@@ -143,12 +158,24 @@ void Sandbox::onRenderImgui()
 
 	ImGui::Begin("Snake Score");
 	ImGui::Text("Score: %d", score);
+	ImGui::Text("Best Score: %d", maxScore);
 	ImGui::End();
 
 	ImGui::Begin("Settings");
 	ImGui::ColorEdit3("Snake color", glm::value_ptr(snakeColor));
 	ImGui::ColorEdit3("Food color", glm::value_ptr(foodColor));
 	ImGui::SliderInt("Grid size", &gridSize, 5, 17);
+	ImGui::End();
+	
+	ImGui::Begin("Start Game");
+	if (ImGui::SmallButton("Start"))
+	{
+		startGame = true;
+	}
+	else if (ImGui::SmallButton("Stop"))
+	{
+		recalculateFreePositions();
+	}
 	ImGui::End();
 }
 
@@ -158,33 +185,77 @@ void Sandbox::onEvent(Hzn::Event& event)
 }
 
 bool Sandbox::isCollidingWithItself(std::pair<int, int> nextPosition) {
-	for (int i = 0; i < snakeSize.size(); i++) {
-		if (nextPosition.first == snakeSize.at(i).first && nextPosition.second == snakeSize.at(i).second)
-			return true;
+	std::pair<int, int> futurePosition = ((snakeHead + nextPosition) % gridSize + std::make_pair(gridSize, gridSize)) % gridSize;
+	if (!freePositions.count(futurePosition))
+	{
+		HZN_DEBUG("Colided with itself!");
+		return true;
 	}
-
 	return false;
 }
 
 std::pair<int, int> Sandbox::generateFood() {
-	std::vector<std::pair<int, int>> positions;
-	for (int i = 0; i < gridSize; i++) {
-		for (int j = 0; j < gridSize; j++) {
-			bool addPosition = true;
-			for (int k = 0; k < snakeSize.size(); k++) {
-				if (snakeSize.at(k).first == i && snakeSize.at(k).second == j)
-					addPosition = false;
-			}
+	auto value = rand() % freePositionsArray.size();
+	return freePositionsArray.at(value);
+}
 
-			if (addPosition) positions.push_back({ i, j });
+void Sandbox::processSnakeInput()
+{
+	if (Hzn::Input::keyPressed(Hzn::Key::Up))
+	{
+		direction = Direction::Up;
+	}
+	else if (Hzn::Input::keyPressed(Hzn::Key::Down))
+	{
+		direction = Direction::Down;
+	}
+	else if (Hzn::Input::keyPressed(Hzn::Key::Left))
+	{
+		direction = Direction::Left;
+	}
+	else if (Hzn::Input::keyPressed(Hzn::Key::Right))
+	{
+		direction = Direction::Right;
+	}
+}
+
+void Sandbox::recalculateFreePositions()
+{
+	freePositions.clear();
+	freePositionsArray.clear();
+	HZN_DEBUG("Recalculating free positions...");
+	for (int i = 0; i < gridSize; ++i)
+	{
+		for (int j = 0; j < gridSize; ++j)
+		{
+			freePositionsArray.push_back({ i, j });
+			freePositions.emplace(std::make_pair(i, j), freePositionsArray.size() - 1);
 		}
 	}
-
-	if (positions.size() == 0) {
-		victory = true;
-
-		return { -1, -1 };
+	resetGame();
+	for (auto& bodygrid : snake)
+	{
+		// retrieve the snake bodygrid position.
+		auto it = freePositions.find(bodygrid);
+		if (it != freePositions.end()) 
+		{
+			freePositions[freePositionsArray.back()] = it->second;
+			// free the previous free position index from the array.
+			swap(freePositionsArray[it->second], freePositionsArray.back());
+			freePositionsArray.pop_back();
+			freePositions.erase(it);
+		}
 	}
+	HZN_DEBUG("free positions: {0}", freePositions.size());
+	oldGridSize = gridSize;
+	startGame = false;
+}
 
-	return positions.at(rand() % positions.size());
+void Sandbox::resetGame()
+{
+	snake = { {0, 0}, {0, 1}, {0, 2} };
+	snakeHead = { 0, 2 };
+	direction = Direction::Up;
+	food = generateFood();
+	score = 0;
 }
