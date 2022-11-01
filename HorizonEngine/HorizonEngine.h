6793e9c83f14c77b422fb87503c3c3956386a6fd
TejasJD <ddusane78@outlook.com>
2022-11-01 11:35:19 +0000

    Added ImGuizmo library, for building Scene Graph Editor UI.

--- HorizonEngine/HorizonEngine.h
+++ HorizonEngine/HorizonEngine.h
@@ -14,6 +14,7 @@
 #include "imgui_internal.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include "ImGuizmo.h"
 
 // Reference additional headers your program requires here.
 #include "HorizonEngine/Core/Core.h"
