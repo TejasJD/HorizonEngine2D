6462784d64b9c32678b9708220262718bff9bd2c
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2022-11-08 21:02:02 +0000

    Added List of Nodes to ImGui Node Editor Box

--- HznApplication/Sandbox.cpp
+++ HznApplication/Sandbox.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include <HorizonEngine.h>
 #include "Sandbox.h"
+#include "imgui_internal.h"
 
 Sandbox::Sandbox()
 	: m_AspectRatio(static_cast<float>(Hzn::App::getApp().getAppWindow().getWidth()) / Hzn::App::getApp().getAppWindow().getHeight()),
@@ -8,17 +9,17 @@ Sandbox::Sandbox()
 {
 	m_Bear = Hzn::Texture2D::create("assets/textures/bear.png");
 	m_SpriteSheet = Hzn::Texture2D::create("assets/sample_game_assets/Tilemap/tilemap_packed.png");
-	m_Sprite = Hzn::Sprite2D::create(m_SpriteSheet, { 0.0f, 0.0f }, {16.0f, 16.0f}, { 1, 1 });
+	m_Sprite = Hzn::Sprite2D::create(m_SpriteSheet, { 0.0f, 0.0f }, { 16.0f, 16.0f }, { 1, 1 });
 }
 
 void Sandbox::onAttach()
 {
-	
+
 }
 
 void Sandbox::onDetach()
 {
-	
+
 }
 
 void Sandbox::onUpdate(Hzn::TimeStep deltaTime)
@@ -35,7 +36,7 @@ void Sandbox::onUpdate(Hzn::TimeStep deltaTime)
 	Hzn::RenderCall::setClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
 	Hzn::RenderCall::submitClear();
 
-	Hzn::Renderer2D::beginScene(dynamic_cast<const Hzn::OrthographicCamera &>(m_CameraController.getCamera()));
+	Hzn::Renderer2D::beginScene(dynamic_cast<const Hzn::OrthographicCamera&>(m_CameraController.getCamera()));
 	// generate gradient across the grid.
 	//for (int32_t i = 0; i < quads; ++i)
 	//{
@@ -58,6 +59,9 @@ void Sandbox::onEvent(Hzn::Event& event)
 	m_CameraController.onEvent(event);
 }
 
+static inline ImVec2 operator+(const ImVec2& lhs, const ImVec2& rhs) { return ImVec2(lhs.x + rhs.x, lhs.y + rhs.y); }
+static inline ImVec2 operator-(const ImVec2& lhs, const ImVec2& rhs) { return ImVec2(lhs.x - rhs.x, lhs.y - rhs.y); }
+
 void Sandbox::onRenderImgui()
 {
 	ImGui::Begin("FPS Counter");
@@ -69,8 +73,8 @@ void Sandbox::onRenderImgui()
 	ImGui::SliderFloat("Quad Angle:", &quadAngle, -180.0f, 180.0f);
 	/*ImGui::Image((ImTextureID)m_Bear->getId(), { 50.0f, 50.0f }, { 0.0f, 1.0f }, { 1.0f, 0.0f });*/
 	auto texCoords = m_Sprite->getTexCoords();
-	ImGui::Image((ImTextureID)m_SpriteSheet->getId(), { 160.0f, 160.0f }, {texCoords[0].x, texCoords[2].y},
-		{texCoords[2].x, texCoords[0].y});
+	ImGui::Image((ImTextureID)m_SpriteSheet->getId(), { 160.0f, 160.0f }, { texCoords[0].x, texCoords[2].y },
+		{ texCoords[2].x, texCoords[0].y });
 	ImGui::End();
 
 	const Hzn::Renderer2DStats& stats = Hzn::Renderer2D::getStats();
@@ -81,4 +85,81 @@ void Sandbox::onRenderImgui()
 	ImGui::Text("Vertices: %d", stats.vertices);
 	ImGui::Text("Indices: %d", stats.indices);
 	ImGui::End();
+
+	// ***** NODE EDITOR *****
+
+	
+	ImGui::Begin("Node Editor");
+	
+	struct Node
+	{
+		int ID;
+		char Name[32];
+		ImVec2 Pos, Size;
+		float Value; 
+		ImVec4 Color;
+		int InputsCount, OutputsCount;
+
+		Node(int id, const char* name, const ImVec2& pos, float value, const ImVec4& color, int inputs_count, int outputs_count) { ID = id; strcpy(Name, name); Pos = pos; Value = value; Color = color; InputsCount = inputs_count; OutputsCount = outputs_count; }
+
+		ImVec2 GetInputSlotPos(int slot_no) const { return ImVec2(Pos.x, Pos.y + Size.y * ((float)slot_no + 1) / ((float)InputsCount + 1)); }
+		ImVec2 GetOutputSlotPos(int slot_no) const { return ImVec2(Pos.x + Size.x, Pos.y + Size.y * ((float)slot_no + 1) / ((float)OutputsCount + 1)); }
+	};
+
+	struct NodeLink
+	{
+		int InputIdx, InputSlot, OutputIdx, OutputSlot;
+
+		NodeLink(int input_idx, int input_slot, int output_idx, int output_slot) { InputIdx = input_idx; InputSlot = input_slot; OutputIdx = output_idx; OutputSlot = output_slot; }
+	};
+
+	// State
+	static ImVector<Node> nodes;
+	static ImVector<NodeLink> links;
+	static ImVec2 scrolling = ImVec2(0.0f, 0.0f);
+	static bool inited = false; 
+	static bool show_grid = true; 
+	static int node_selected = -1; 
+
+	ImGuiIO& io = ImGui::GetIO();
+	if (!inited)
+	{
+		nodes.push_back(Node(0, "Test Node 1", ImVec2(40, 50), 0.5f, ImColor(255, 100, 100), 1, 1));
+		nodes.push_back(Node(1, "Test Node 2", ImVec2(40, 150), 0.42f, ImColor(200, 100, 200), 1, 1));
+		nodes.push_back(Node(2, "Test Node 3", ImVec2(270, 80), 1.0f, ImColor(0, 200, 100), 2, 2));
+		links.push_back(NodeLink(0, 0, 2, 0));
+		links.push_back(NodeLink(1, 0, 2, 1));
+		inited = true;
+	}
+
+	// Nodes on the LHS 
+	
+	bool open_context_menu = false; 
+	int node_hovered_in_list = -1;
+	int node_hovered_in_scene = -1;
+	ImGui::BeginChild("node_list", ImVec2(100, 0));
+	ImGui::Text("List of Nodes");
+	ImGui::Separator();
+	for (int node_idx = 0; node_idx < nodes.Size; node_idx++)
+	{
+		Node* node = &nodes[node_idx];
+		ImGui::PushID(node->ID);
+		if (ImGui::Selectable(node->Name, node->ID == node_selected))
+			node_selected = node->ID;
+		if (ImGui::IsItemHovered())
+		{
+			node_hovered_in_list = node->ID;
+			open_context_menu |= ImGui::IsMouseClicked(1);
+		}
+		ImGui::PopID();
+	}
+	ImGui::EndChild();
+	
+	ImGui::End();
+	
+
+
+
+
 }
+
