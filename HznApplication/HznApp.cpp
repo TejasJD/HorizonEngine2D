ced694e8e65a9a23eb61481fc11e180bb5580884
prajakta kulkarni <kulkarni.prajakta2612@gmail.com>
2022-11-10 12:06:15 +0000

    Changed files for view functionality of toolbar

--- HznApplication/HznApp.cpp
+++ HznApplication/HznApp.cpp
@@ -30,12 +30,8 @@ EditorLayer::EditorLayer(const char* name) : Hzn::Layer(name) {
 	}
 }
 
-
-
 void EditorLayer::onAttach()
 {
-	
-
 	HZN_INFO("Editor Layer Attached!");
 	Hzn::ProjectFile *file = new Hzn::ProjectFile("assets/scenes/input.txt");
 	openScene = new Hzn::Scene(file);
@@ -53,74 +49,6 @@ void EditorLayer::onRenderImgui()
 	ImGui::SetNextWindowSize(ImVec2((float)vWindowSize.x, (float)vWindowSize.y));
 
 	static bool my_tool_active = true;
-
-	//if (ImGui::Begin(
-	//	"From Editor Layer",
-	//	nullptr,
-	//	ImGuiWindowFlags_MenuBar |
-	//	ImGuiWindowFlags_NoResize |
-	//	ImGuiWindowFlags_NoMove |
-	//	ImGuiWindowFlags_NoCollapse |
-	//	ImGuiWindowFlags_NoBringToFrontOnFocus |
-	//	ImGuiWindowFlags_NoTitleBar
-	//)
-	//	)
-
-	//{
-	//	{
-	//		static const ImGuiDockNodeFlags dockspaceFlags = ImGuiDockNodeFlags_None;
-	//		ImGuiID dockSpace = ImGui::GetID("MainWindowDockspace");
-	//		ImGui::DockSpace(dockSpace, ImVec2(0.0f, 0.0f), dockspaceFlags);
-
-	//		if (ImGui::BeginMainMenuBar())
-	//		{
-	//			if (ImGui::BeginMenu("File"))
-	//			{
-	//				if (ImGui::MenuItem("Open", "Ctrl+O")) { /* Do Something */ }
-	//				if (ImGui::MenuItem("Save", "Ctrl+S")) { /* Do Something */ }
-	//				if (ImGui::MenuItem("Close", "Ctrl+W")) { my_tool_active = false; }
-	//				ImGui::EndMenu();
-	//			}
-	//			if (ImGui::BeginMenu("Edit"))
-	//			{
-	//				if(ImGui::MenuItem("Undo", "Ctrl+Z")) { /* Do Something */ }
-	//				if(ImGui::MenuItem("Redo", "Ctrl+Y")) { /* Do Something */ }
-
-	//				if(ImGui::MenuItem("Cut", "Ctrl+X")){ /*Do Something*/ }
-	//				if (ImGui::MenuItem("Copy", "Ctrl+C")) { /*Do Something*/ }
-	//				if (ImGui::MenuItem("Paste", "Ctrl+V")) { /*Do Something*/ }
-	//				ImGui::EndMenu();
-	//			}
-	//			if (ImGui::BeginMenu("Assets"))
-	//			{
-	//				if(ImGui::MenuItem("Asset Submenu_1")){ /* Do Something */ }
-	//				ImGui::EndMenu();
-	//			}
-	//			if (ImGui::BeginMenu("Game Objects"))
-	//			{
-	//				if (ImGui::MenuItem("Game Object Submenu_1")) { /* Do Something */ }
-	//				ImGui::EndMenu();
-	//			}
-	//			if (ImGui::BeginMenu("Components"))
-	//			{
-	//				if (ImGui::MenuItem("Components Submenu_1")) { /* Do Something */ }
-	//				ImGui::EndMenu();
-	//			}
-	//			if (ImGui::BeginMenu("Window"))
-	//			{
-	//				if (ImGui::MenuItem("Window Submenu_1")) { /* Do Something */ }
-	//				ImGui::EndMenu();
-	//			}
-	//			if (ImGui::BeginMenu("Help"))
-	//			{
-	//				if (ImGui::MenuItem("Help Submenu_1")) { /* Do Something */ }
-	//				ImGui::EndMenu();
-	//			}
-	//		}
-	//		ImGui::EndMainMenuBar();
-	//	}
-	//}
-	//ImGui::End();
 
 	// Setup dock space
 	bool pOpen = true;
