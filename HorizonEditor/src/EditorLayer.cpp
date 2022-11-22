e34caedcdb279cedfbd608219d95860b97951b27
alex-mulkerrins <D21126666@mytudublin.ie>
2022-11-22 17:11:30 +0000

    refactor editor Layer - commit popup modals

--- HorizonEditor/src/EditorLayer.cpp
+++ HorizonEditor/src/EditorLayer.cpp
@@ -89,91 +89,11 @@ void EditorLayer::onEvent(Hzn::Event& e)
 	}
 }
 
-//void EditorLayer::openProject()
-//{
-//	projectRootFolder = EditorData::m_Project_Active->getPath().parent_path().string();
-//	//set current path of is  project root directory
-//	m_CurrentDirectory = projectRootFolder;
-//
-//	std::string iconPath = projectRootFolder + "\\icons";
-//
-//	currentScenePath = (EditorData::s_Scene_Active ? EditorData::s_Scene_Active->getFilePath().string() : std::string());
-//
-//	for (const auto& entry : std::filesystem::recursive_directory_iterator(iconPath)) {
-//		//create texture of directory icon
-//		if (!entry.is_directory() && entry.path().string().find("DirectoryIcon.png") != std::string::npos) {
-//			folderIcon = Hzn::Texture2D::create(entry.path().string());
-//		}
-//
-//		//create texture of file icon
-//		if (!entry.is_directory() && entry.path().string().find("FileIcon.png") != std::string::npos) {
-//
-//			fileIcon = Hzn::Texture2D::create(entry.path().string());
-//		}
-//	}
-//
-//	//create map to store format of sprite
-//	std::map<std::string, std::string> spriteFormat;
-//
-//	//create texture for image file and sprites from sprite sheets
-//	for (const auto& entry : std::filesystem::recursive_directory_iterator(projectRootFolder))
-//	{
-//
-//		/*if (!entry.is_directory() && entry.path().parent_path().string().find("audios") != std::string::npos)
-//		{
-//			assetManager.LoadAudio(entry.path().string(), entry.path().string());
-//		}*/
-//
-//		if (entry.path().string().find("icons") != std::string::npos)
-//		{
-//			continue;
-//		}
-//
-//
-//		if (!entry.is_directory() && entry.path().string().find(".png") != std::string::npos) {
-//			assetManager.LoadTexture(entry.path().string(), entry.path().string());
-//		}
-//
-//		if (!entry.is_directory() && entry.path().parent_path().string().find("sprites") != std::string::npos && entry.path().string().find(".png") != std::string::npos) {
-//
-//			for (const auto& metaFile : std::filesystem::recursive_directory_iterator(entry.path().parent_path())) {
-//
-//				if (metaFile.path().string().find(".meta") != std::string::npos && metaFile.path().filename().string().substr(0, metaFile.path().filename().string().find(".")) == entry.path().filename().string().substr(0, entry.path().filename().string().find("."))) {
-//					std::ifstream infile(metaFile.path().c_str(), std::ifstream::binary);
-//					std::string line;
-//
-//					while (std::getline(infile, line)) {
-//						std::istringstream is_line(line);
-//						std::string key;
-//						if (std::getline(is_line, key, ':'))
-//						{
-//							std::string value;
-//
-//							if (std::getline(is_line, value))
-//							{
-//								spriteFormat[key] = value;
-//							}
-//						}
-//					}
-//
-//				}
-//			}
-//
-//			auto spriteSheet = Hzn::Texture2D::create(entry.path().string());
-//
-//
-//			for (size_t i = 0; i < std::stoi(spriteFormat.find("row")->second); i++)
-//			{
-//				for (size_t j = 0; j < std::stoi(spriteFormat.find("column")->second); j++)
-//				{
-//					std::string currentSprite = "(" + std::to_string(i) + "," + std::to_string(j) + ")";
-//					spriteMap.insert(std::make_pair(entry.path().string().append(currentSprite), Hzn::Sprite2D::create(spriteSheet, { i, j }, { std::stof(spriteFormat.find("width")->second),std::stof(spriteFormat.find("height")->second) })));
-//				}
-//			}
-//
-//		}
-//	}
-//}
+
+
+
+
+
 //// ***** CODE FOR NODE EDITOR *****
 //template <typename T, std::size_t N>
 //
@@ -536,6 +456,7 @@ void EditorLayer::onRenderImgui()
 		ImGui::EndMenuBar();
 	}//End options
 
+	//MODALS
 	//Scene pop-ups
 	Modals::getCenterWindow();
 	//new project
@@ -544,86 +465,8 @@ void EditorLayer::onRenderImgui()
 	Modals::getNewScenePopup();
 
 
-	////modal view - gets main viewport, which is window itself
-	//ImVec2 center = ImGui::GetMainViewport()->GetCenter();
-	//ImGui::SetNextWindowPos(center, ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
 
-	////Begin New Project - popup modal
-	//if (request_NewProject)
-	//	ImGui::OpenPopup("New Project");
 
-	//
-	//if (ImGui::BeginPopupModal("New Project", &request_NewProject, ImGuiWindowFlags_AlwaysAutoResize))
-	//{
-	//	ImGui::InputText("Project Name", projectNameBuffer, 512);
-	//	ImGui::InputText("Root Folder", directoryPathBuffer, 1024);
-	//	ImGui::SameLine();
-	//	if (ImGui::Button("..."))
-	//	{
-	//		strcpy_s(directoryPathBuffer, Hzn::FileDialogs::openFolder().c_str());
-	//	}
-	//	if (!canCreateProject) {
-	//		ImGui::Text("Enter valid name / choose valid base directory / Project with the same name exists in the same directory!");
-	//	}
-
-	//	// create new project and set it as active project.
-	//	std::string nameString = projectNameBuffer;
-	//	std::filesystem::path directoryPath = directoryPathBuffer;
-
-	//	if (!nameString.empty() && !directoryPath.empty())
-	//	{
-	//		if (std::filesystem::exists(directoryPathBuffer) &&
-	//			std::filesystem::is_directory(directoryPathBuffer) &&
-	//			!std::filesystem::exists(directoryPath.string() + "\\" + projectNameBuffer))
-	//		{
-	//			canCreateProject = true;
-	//		}
-	//		else canCreateProject = false;
-	//	}
-	//	else canCreateProject = false;
-
-	//	if (ImGui::Button("Create", ImVec2(120, 0)))
-	//	{
-	//			if (canCreateProject) {
-	//				EditorData::m_Project_Active = Hzn::ProjectManager::create(projectNameBuffer, std::filesystem::path(directoryPathBuffer));
-	//				EditorData::s_Scene_Active = EditorData::m_Project_Active->getActiveScene();
-
-	//				// clear the directory path buffers.
-	//				memset(projectNameBuffer, '\0', sizeof(projectNameBuffer));
-	//				memset(directoryPathBuffer, '\0', sizeof(directoryPathBuffer));
-	//				openProject();
-	//			}
-	//		ImGui::CloseCurrentPopup();
-	//		request_NewProject = false;
-	//	}
-	//	ImGui::EndPopup();
-	//}//End new project
-
-	////Begin new scene - pop up modal
-	//if (request_NewScene)
-	//	ImGui::OpenPopup("New Scene");
-
-	//if (ImGui::BeginPopupModal("New Scene", &request_NewScene, ImGuiWindowFlags_AlwaysAutoResize))
-	//{
-
-	//	ImGui::InputText("Scene Name", sceneNameBuffer, 256);
-	//	std::string sceneName = std::string(sceneNameBuffer);
-
-	//	if (sceneName.empty()) ImGui::Text("scene name field is empty!");
-
-	//	ImGui::Separator();
-	//	if (ImGui::Button("Create", ImVec2(120, 0)))
-	//	{
-	//		// create new project and set it as active project.
-	//		if (!sceneName.empty()) {
-	//			Hzn::ProjectManager::newScene(sceneName);
-	//			EditorData::s_Scene_Active = EditorData::m_Project_Active->getActiveScene();
-	//			memset(sceneNameBuffer, '\0', sizeof(sceneNameBuffer));
-	//			request_NewScene = false;
-	//		}
-	//	}
-	//	ImGui::EndPopup();
-	//}//END New Scene
 
 	// OBJECT HIERARCHY BEGIN
 	drawHierarchy();
@@ -995,7 +838,7 @@ void EditorLayer::drawObjects(Hzn::GameObject& object)
 
 	if (ImGui::BeginDragDropTarget()) {
 		if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("HIERARCHY_PAYLOAD")) {
-			Hzn::GameObject receivedObject = m_Scene->getGameObject((uint32_t) * (const int*)payload->Data);
+			Hzn::GameObject receivedObject = EditorData::s_Scene_Active->getGameObject((uint32_t) * (const int*)payload->Data);
 			receivedObject.setParent(object);
 		}
 
