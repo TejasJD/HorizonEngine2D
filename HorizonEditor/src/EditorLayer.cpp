9b245310a42255c8d7adb51aaead0f00fca96625
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2022-11-24 15:18:34 +0000

    Sample Node Editor Using ImNodes

--- HorizonEditor/src/EditorLayer.cpp
+++ HorizonEditor/src/EditorLayer.cpp
@@ -93,227 +93,6 @@ void EditorLayer::onEvent(Hzn::Event& e)
 }
 
 
-
-
-
-
-//// ***** CODE FOR NODE EDITOR *****
-//template <typename T, std::size_t N>
-//
-//struct Array
-//{
-//	T data[N];
-//	const size_t size() const { return N; }
-//
-//	const T operator [] (size_t index) const { return data[index]; }
-//	operator T* () {
-//		T* p = new T[N];
-//		memcpy(p, data, sizeof(data));
-//		return p;
-//	}
-//};
-//
-//template <typename T, typename ... U> Array(T, U...)->Array<T, 1 + sizeof...(U)>;
-//
-//struct GraphEditorDelegate : public GraphEditor::Delegate
-//{
-//	bool AllowedLink(GraphEditor::NodeIndex from, GraphEditor::NodeIndex to) override
-//	{
-//		return true;
-//	}
-//
-//	void SelectNode(GraphEditor::NodeIndex nodeIndex, bool selected) override
-//	{
-//		mNodes[nodeIndex].mSelected = selected;
-//	}
-//
-//	void MoveSelectedNodes(const ImVec2 delta) override
-//	{
-//		for (auto& node : mNodes)
-//		{
-//			if (!node.mSelected)
-//			{
-//				continue;
-//			}
-//			node.x += delta.x;
-//			node.y += delta.y;
-//		}
-//	}
-//
-//	virtual void RightClick(GraphEditor::NodeIndex nodeIndex, GraphEditor::SlotIndex slotIndexInput, GraphEditor::SlotIndex slotIndexOutput) override
-//	{
-//	}
-//
-//	void AddLink(GraphEditor::NodeIndex inputNodeIndex, GraphEditor::SlotIndex inputSlotIndex, GraphEditor::NodeIndex outputNodeIndex, GraphEditor::SlotIndex outputSlotIndex) override
-//	{
-//		mLinks.push_back({ inputNodeIndex, inputSlotIndex, outputNodeIndex, outputSlotIndex });
-//	}
-//
-//	void DelLink(GraphEditor::LinkIndex linkIndex) override
-//	{
-//		mLinks.erase(mLinks.begin() + linkIndex);
-//	}
-//
-//	void CustomDraw(ImDrawList* drawList, ImRect rectangle, GraphEditor::NodeIndex nodeIndex) override
-//	{
-//		drawList->AddLine(rectangle.Min, rectangle.Max, IM_COL32(0, 0, 0, 255));
-//		drawList->AddText(rectangle.Min, IM_COL32(255, 128, 64, 255), "Draw");
-//	}
-//
-//	const size_t GetTemplateCount() override
-//	{
-//		return sizeof(mTemplates) / sizeof(GraphEditor::Template);
-//	}
-//
-//	const GraphEditor::Template GetTemplate(GraphEditor::TemplateIndex index) override
-//	{
-//		return mTemplates[index];
-//	}
-//
-//	const size_t GetNodeCount() override
-//	{
-//		return mNodes.size();
-//	}
-//
-//	const GraphEditor::Node GetNode(GraphEditor::NodeIndex index) override
-//	{
-//		const auto& myNode = mNodes[index];
-//		return GraphEditor::Node
-//		{
-//			myNode.name,
-//			myNode.templateIndex,
-//			ImRect(ImVec2(myNode.x, myNode.y), ImVec2(myNode.x + 200, myNode.y + 200)),
-//			myNode.mSelected
-//		};
-//	}
-//
-//	const size_t GetLinkCount() override
-//	{
-//		return mLinks.size();
-//	}
-//
-//	const GraphEditor::Link GetLink(GraphEditor::LinkIndex index) override
-//	{
-//		return mLinks[index];
-//	}
-//
-//	// Graph datas
-//	static const inline GraphEditor::Template mTemplates[] = {
-//		{
-//			IM_COL32(160, 160, 180, 255),
-//			IM_COL32(100, 100, 140, 255),
-//			IM_COL32(110, 110, 150, 255),
-//			1,
-//			Array{"MyInput"},
-//			nullptr,
-//			2,
-//			Array{"MyOutput0", "MyOuput1"},
-//			nullptr
-//		},
-//
-//		{
-//			IM_COL32(180, 160, 160, 255),
-//			IM_COL32(140, 100, 100, 255),
-//			IM_COL32(150, 110, 110, 255),
-//			3,
-//			nullptr,
-//			Array{ IM_COL32(200,100,100,255), IM_COL32(100,200,100,255), IM_COL32(100,100,200,255) },
-//			1,
-//			Array{"MyOutput0"},
-//			Array{ IM_COL32(200,200,200,255)}
-//		}
-//	};
-//
-//	struct Node
-//	{
-//		const char* name;
-//		GraphEditor::TemplateIndex templateIndex;
-//		float x, y;
-//		bool mSelected;
-//	};
-//
-//	std::vector<Node> mNodes = {
-//		{
-//			"Start Node",
-//			0,
-//			0, 0,
-//			false
-//		},
-//
-//		{
-//			"End Node",
-//			0,
-//			400, 0,
-//			false
-//		},
-//
-//		{
-//			"My Node 2",
-//			1,
-//			400, 400,
-//			false
-//		}
-//	};
-//
-//	std::vector<GraphEditor::Link> mLinks = { {0, 0, 1, 0} };
-//};//End node editor
-
-
-
-
-
-//			2,
-//			Array{"MyOutput0", "MyOuput1"},
-//			nullptr
-//		},
-//
-//		{
-//			IM_COL32(180, 160, 160, 255),
-//			IM_COL32(140, 100, 100, 255),
-//			IM_COL32(150, 110, 110, 255),
-//			3,
-//			nullptr,
-//			Array{ IM_COL32(200,100,100,255), IM_COL32(100,200,100,255), IM_COL32(100,100,200,255) },
-//			1,
-//			Array{"MyOutput0"},
-//			Array{ IM_COL32(200,200,200,255)}
-//		}
-//	};
-//
-//	struct Node
-//	{
-//		const char* name;
-//		GraphEditor::TemplateIndex templateIndex;
-//		float x, y;
-//		bool mSelected;
-//	};
-//
-//	std::vector<Node> mNodes = {
-//		{
-//			"Start Node",
-//			0,
-//			0, 0,
-//			false
-//		},
-//
-//		{
-//			"End Node",
-//			0,
-//			400, 0,
-//			false
-//		},
-//
-//		{
-//			"My Node 2",
-//			1,
-//			400, 400,
-//			false
-//		}
-//	};
-//
-//	std::vector<GraphEditor::Link> mLinks = { {0, 0, 1, 0} };
-//};
-
 void EditorLayer::onRenderImgui()
 {
 	/*static bool showDemo = true;
@@ -637,49 +416,66 @@ void EditorLayer::onRenderImgui()
 	ImGui::End();
 	// CONTENT BROWSER END
 
-	//// VISUAL SCRIPTING BEGIN 
-	//static GraphEditor::Options options;
-	////static GraphEditorDelegate delegate;
-	//static GraphEditor::ViewState viewState;
-	//static GraphEditor::FitOnScreen fit = GraphEditor::Fit_None;
-	//static bool showGraphEditor = true;
-
-	//if (ImGui::CollapsingHeader("Graph Editor"))
-	//{
-	//	ImGui::Checkbox("Show GraphEditor", &showGraphEditor);
-	//	GraphEditor::EditOptions(options);
-	//}
-
-	//if (showGraphEditor)
-	//{
-	//	ImGui::Begin("Graph Editor", NULL, 0);
-	//	if (ImGui::Button("Fit all nodes"))
-	//	{
-	//		fit = GraphEditor::Fit_AllNodes;
-	//	}
-	//	ImGui::SameLine();
-	//	if (ImGui::Button("Fit selected nodes"))
-	//	{
-	//		fit = GraphEditor::Fit_SelectedNodes;
-	//	}
-	//	//GraphEditor::Show(delegate, options, viewState, true, &fit);
-
-	//	ImGui::End();
-	//}
-	//// VISUAL SCRIPTING END 
-
-	const int hardcoded_node_id = 1;
-
+	std::vector<std::pair<int, int>> links;
+	// elsewhere in the code...
+	for (int i = 0; i < links.size(); ++i)
+	{
+		const std::pair<int, int> p = links[i];
+		// in this case, we just use the array index of the link
+		// as the unique identifier
+		ImNodes::Link(i, p.first, p.second);
+	}
 	ImGui::Begin("Node Editor");
-
 	ImNodes::BeginNodeEditor();
 
-	ImNodes::BeginNode(hardcoded_node_id);
-	ImGui::Dummy(ImVec2(80.0f, 45.0f));
+	ImNodes::BeginNode(1);
+	ImNodes::BeginNodeTitleBar();
+	ImGui::TextUnformatted("Start Game Event");
+	ImNodes::EndNodeTitleBar();
+	ImNodes::BeginOutputAttribute(1);
+	ImGui::Text("Output Pin");
+	ImNodes::EndOutputAttribute();
 	ImNodes::EndNode();
 
-	ImNodes::EndNodeEditor();
+	ImNodes::BeginNode(2);
+	ImNodes::BeginNodeTitleBar();
+	ImGui::TextUnformatted("Keyboard Event");
+	ImNodes::EndNodeTitleBar();
+	ImNodes::BeginInputAttribute(1);
+	ImGui::Text("Input Pin");
+	ImNodes::EndInputAttribute();
+	ImNodes::BeginOutputAttribute(2);
+	ImGui::Text("Output Pin");
+	ImNodes::EndOutputAttribute();
+	ImNodes::EndNode();
 
+	ImNodes::BeginNode(3);
+	ImNodes::BeginNodeTitleBar();
+	ImGui::TextUnformatted("Mouse Event");
+	ImNodes::EndNodeTitleBar();
+	/*
+
+	*/
+	ImNodes::EndNode();
+
+	ImNodes::BeginNode(4);
+	ImNodes::BeginNodeTitleBar();
+	ImGui::TextUnformatted("If");
+	ImNodes::EndNodeTitleBar();
+	/*
+
+	*/
+	ImNodes::EndNode();
+	
+	
+
+	ImNodes::MiniMap(0.2f, ImNodesMiniMapLocation_BottomRight);
+	ImNodes::EndNodeEditor();
+	int start_attr, end_attr;
+	if (ImNodes::IsLinkCreated(&start_attr, &end_attr))
+	{
+		links.push_back(std::make_pair(start_attr, end_attr));
+	}
 	ImGui::End();
 
 	// VIEWPORT BEGIN
